2a3f68db0d0a806831ffa19fcdf66dc578311af9
PARTH <parthsinghal23.cs@gmail.com>
2024-10-21 15:31:48 +0530

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
         src="src\components\assets\logo.png"
         alt="logo"
       />
-      <h1 className="text-3xl font-bold capitalize">Dr. BR Ambedkar Univ Museum</h1>
+      <h1 className="text-3xl font-bold capitalize">Sanskritik Dharohar evam Sangrahlaya</h1>
       <div className="flex items-center align-middle gap-5">
       <TiThMenu />
       <h3 className="font-light text-3xl">MENU</h3>
